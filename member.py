685c75da72ae80d9e0aaa8efcb67258c4ca8ea76
imamura2293 <uu77ee22dd55>
2020-11-01 21:39:27 +0900

    class

---
+++ member.py
@@ -0,0 +1,10 @@
+class Member:
+    def __init__(self, first_name: str, family_name: str):
+        self.first_name = first_name
+        self.family_name = family_name
+
+bob = Member(first_name="bob", family_name="Martin")
+
+
+print(bob.first_name)
+print(bob.family_name)
