73c0f12735ef678dcec9157f2e8d3b89a4e7a50a
imamura2293 <uu77ee22dd55>
2020-11-01 21:47:19 +0900

    新たなインスタンス

--- member.py
+++ member.py
@@ -3,15 +3,21 @@ class Member:
         self.first_name = first_name
         self.family_name = family_name
 
-bob = Member(first_name="bob", family_name="Martin")
 
+bob = Member(first_name="bob", family_name="Martin")
 
 print(bob.first_name)
 print(bob.family_name)
 
+# what type of bob
 print(type(bob))
 
 print(type("hello"))
 
 print(type(345))
 
+tom = Member(first_name="tom", family_name="Ford")
+
+print(type(tom))
+print(tom.first_name)
+print(tom.family_name)
